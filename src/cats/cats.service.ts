1e919314ba087f7664a9232635ed8488f90e4638
sirojiddin <sirojiddin.ismoilov7181@gmail.com>
2022-06-28 15:26:33 +0500

    second commit

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize'
+import { resolve } from 'path';
 import { CreateCatDto } from './dto/createDto-cat.dto';
 import { UpdateCatDto } from './dto/updateCat.dto';
 import { Cat } from './models/cat.model';
@@ -7,7 +8,7 @@ import { Cat } from './models/cat.model';
 @Injectable()
 export class CatsService {
 
-  constructor(@InjectModel(Cat) private readonly catModel: typeof Cat) {}
+  constructor(@InjectModel(Cat) private readonly catModel: typeof Cat) { }
 
   getOne(id: number): Promise<Cat> {
     return this.catModel.findOne({
@@ -25,16 +26,33 @@ export class CatsService {
     return this.catModel.create({ ...createCatDto });
   }
 
-  edit(id: number, updateCatDto: UpdateCatDto){
-    return this.catModel.update(updateCatDto,{
+  edit(id: number, updateCatDto: UpdateCatDto) {
+    return this.catModel.update(updateCatDto, {
       where: {
         catId: id,
       }
     });
   }
 
-  async delete(id: number): Promise<void>{
+  async delete(id: number): Promise<void> {
     const cat = await this.getOne(id);
+    if (!cat) {
+      throw new NotFoundException('Not Found');
+    }
     await cat.destroy();
   }
+
+  // delete(id: number) {
+  //   return new Promise((resolve, rej) => {
+  //     this.getOne(id).then(cat => {
+  //       cat.destroy().then(() => {
+  //         resolve(null);
+  //       }).catch(err => {
+  //         rej(new InternalServerErrorException('Database error'))
+  //       })
+  //     }).catch(err => {
+  //       rej(new NotFoundException('Cat not found'))
+  //     })
+  //   })
+  // }
 }
